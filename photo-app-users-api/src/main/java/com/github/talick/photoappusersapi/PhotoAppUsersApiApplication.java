56f683c8f1f9450f1433c08888b60ef44694e98a
talick <talgat.asanbaev@gmail.com>
2021-06-10 20:02:13 +0600

    Add Feign and Hystrix

--- photo-app-users-api/src/main/java/com/github/talick/photoappusersapi/PhotoAppUsersApiApplication.java
+++ photo-app-users-api/src/main/java/com/github/talick/photoappusersapi/PhotoAppUsersApiApplication.java
@@ -4,6 +4,7 @@ import com.github.talick.photoappusersapi.shared.FeignErrorDecoder;
 import feign.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -14,6 +15,7 @@ import org.springframework.web.client.RestTemplate;
 @EnableEurekaClient
 @SpringBootApplication
 @EnableFeignClients
+@EnableCircuitBreaker
 public class PhotoAppUsersApiApplication {
 
     public static void main(String[] args) {
