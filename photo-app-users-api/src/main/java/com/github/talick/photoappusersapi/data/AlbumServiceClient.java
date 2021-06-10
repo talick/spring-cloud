53ba8eec37e873d81837896f73f0568a1d6afa51
talick <talgat.asanbaev@gmail.com>
2021-06-10 22:47:38 +0600

    fallbackFactory for Feign

--- photo-app-users-api/src/main/java/com/github/talick/photoappusersapi/data/AlbumServiceClient.java
+++ photo-app-users-api/src/main/java/com/github/talick/photoappusersapi/data/AlbumServiceClient.java
@@ -1,15 +1,19 @@
 package com.github.talick.photoappusersapi.data;
 
 import com.github.talick.photoappusersapi.ui.model.AlbumResponseModel;
+import feign.FeignException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.openfeign.FallbackFactory;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
-@FeignClient(name = "albums-ws", fallback = AlbumsFallback.class)
+@FeignClient(name = "albums-ws", fallbackFactory = AlbumsFallbackFactory.class)
 public interface AlbumServiceClient {
 
     @GetMapping("/users/{id}/albums")
@@ -17,10 +21,32 @@ public interface AlbumServiceClient {
 }
 
 @Component
-class AlbumsFallback implements AlbumServiceClient {
+class AlbumsFallbackFactory implements FallbackFactory<AlbumServiceClient> {
+
+
+    @Override
+    public AlbumServiceClient create(Throwable cause) {
+
+        return new AlbumsServiceClientFallback(cause);
+    }
+}
+
+class AlbumsServiceClientFallback implements AlbumServiceClient {
+    Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    private final Throwable cause;
+
+    AlbumsServiceClientFallback(Throwable cause) {
+        this.cause = cause;
+    }
 
     @Override
     public List<AlbumResponseModel> getAlbums(String id) {
-        return new ArrayList<>();
+        if (cause instanceof FeignException && ((FeignException) cause).status() == 404) {
+            logger.error("404 error took place when getAlbums was called with userId; " + id + ". Error message: " + cause.getLocalizedMessage());
+        } else {
+            logger.error("Other error took place: " + cause.getLocalizedMessage());
+        }
+        return Collections.emptyList();
     }
 }
